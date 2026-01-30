5ec6892611266b5b671933cb467d901c7e62dfce
kaushalsharma97299@gmail.com <115640134+kaushal97299@users.noreply.github.com>
2026-01-30 14:35:30 +0530

    ui change12

--- app/dashboard/[id]/book/page.tsx
+++ app/dashboard/[id]/book/page.tsx
@@ -150,8 +150,7 @@ const router = useRouter();
             </button>
             <button
               onClick={() => {
-  router.push(
-    `#?fullName=${fullName}&phone=${phone}&email=${email}&license=${licenseNo}&car=${car.name}&fuel=${car.fuel}&gear=${car.gear}&pickupDate=${pickupDate}&pickupTime=${pickupTime}&dropDate=${dropDate}&dropTime=${dropTime}&days=${days}&total=${totalPrice}`
+  router.push(`/payment?fullName=${fullName}&phone=${phone}&email=${email}&license=${licenseNo}&car=${car.name}&fuel=${car.fuel}&gear=${car.gear}&pickupDate=${pickupDate}&pickupTime=${pickupTime}&dropDate=${dropDate}&dropTime=${dropTime}&days=${days}&total=${totalPrice}`
   );
 }}
               className="flex-1 bg-indigo-600 text-white rounded-xl py-2 font-bold"
